060cf5b69b70966b6dcb7b333b4de9988592d37e
Cristiano <kristian0tj2019@gmail.com>
2023-02-21 16:14:45 -0300

    refactor: create db user user.route user.service

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,21 @@
-const create = (req, res) => {
+const mongoose = require('mongoose');
+const userService = require('../services/user.service');
+
+const create = async (req, res) => {
     const {name, username, email, password, avatar, background} = req.body;
         if (!name || !username || !email || !password || !avatar || !background) {
             res.status(400).send({message: "Submit all fields for registration"});
         }
+    
+    const user = await userService.createService(req.body);
+
+    if (!user) {
+        return res.status(400).send({ message: 'Error creating User' });
+    }
     res.status(201).send({
         message: "User created successfully",
         user: {
+            id: user._id,
             name,
             username,
             email,
@@ -15,4 +25,30 @@ const create = (req, res) => {
     });
 };
 
-module.exports = { create };
+const findAll = async (req, res) => {
+    const users = await userService.findAllService();
+
+    if (users.length === 0) {
+        return res.status(400).send({ message: 'There are no registered users' });
+    }
+
+    res.send(users)
+};
+
+const findById = async (req, res) => {
+    const id = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send({ message: 'Invalid ID' });
+    }
+
+    const user = await userService.findByIdService(id);
+
+    if (!user) {
+        return res.status(400).send({ message: 'User not found' });
+    }
+
+    res.send(user);
+};
+
+module.exports = { create, findAll, findById };
---
+++ src/database/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const connectDatabase = () => {
+    console.log('Wait connecting to the database')
+
+    mongoose.set("strictQuery", false);
+
+    mongoose
+        .connect(
+        "mongodb://root:root@ac-wsxagob-shard-00-00.znnbqm2.mongodb.net:27017,ac-wsxagob-shard-00-01.znnbqm2.mongodb.net:27017,ac-wsxagob-shard-00-02.znnbqm2.mongodb.net:27017/?ssl=true&replicaSet=atlas-3ah88u-shard-0&authSource=admin&retryWrites=true&w=majority",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
+    .then(() => console.log("MongoDB Atlas Connected"))
+    .catch((error) => console.log(error));
+};
+
+module.exports = connectDatabase;
